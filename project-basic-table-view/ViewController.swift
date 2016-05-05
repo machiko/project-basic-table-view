3d352c3738a52998f0570848df7732a3005ad08a
reyes <machikoreo@gmail.com>
2016-05-05 18:43:01 +0800

    - add refresh controller

--- project-basic-table-view/ViewController.swift
+++ project-basic-table-view/ViewController.swift
@@ -14,13 +14,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     @IBOutlet weak var foodTableVeiw: UITableView!
     
     var foodItems = NSMutableArray()
+    var foodOtherItems = NSMutableArray()
+    var refreshControl: UIRefreshControl?
+    let alertController = UIAlertController(title: "Oops！", message: "小當家沒菜了", preferredStyle: .Alert)
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         foodItems = ["黃金炒飯", "迷魔幻麻婆豆腐", "炒青江菜", "南鮮青椒肉絲", "畢拉夫什錦炒飯", "番茄燴炒飯", "叫化子雞"]
+        foodOtherItems = ["鳳凰水晶", "紅油水餃", "冕頂餃子", "火焰鍋貼", "精神百倍麵"]
+        
+        // 在 nav bar 左邊加入 edit button
         navigationItem.leftBarButtonItem = editButtonItem()
-//        self.foodTableVeiw.editing = true
+        
+        // 讓 refreshControl 增加一個 target，然後指定哪一個 view 要執行 action，在什麼事件觸發
+        refreshControl = UIRefreshControl()
+        refreshControl!.addTarget(self, action: #selector(ViewController.refreshFoodItem), forControlEvents: UIControlEvents.ValueChanged)
+        foodTableVeiw.addSubview(refreshControl!)
+        
+        // alert 視窗 style
+        alertController.addAction(UIAlertAction(title: "呿", style: .Default, handler: nil))
     }
 
     override func didReceiveMemoryWarning() {
@@ -41,32 +55,36 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return cell
     }
     
-    // conditional editing of the table view.
-//    func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
-//        return true
-//    }
-    
     // editing the table view.
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
-//            supermanItems.removeObjectAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
     }
     
-//    add editing action
+    // 將 edit action 與 foodTableView 關聯
     override func setEditing(editing: Bool, animated: Bool) {
         super.setEditing(editing, animated: animated)
         self.foodTableVeiw.setEditing(editing, animated: animated)
     }
     
-    
-//    func tableView(tableView: UITableView, moveRowAtIndexPath sourceIndexPath: NSIndexPath, toIndexPath destinationIndexPath: NSIndexPath) {
-//            
-//    }
-
+    func refreshFoodItem() {
+        if let rand: Int? = Int(arc4random_uniform(UInt32(foodOtherItems.count))) {
+            // 當沒有菜可以增加時，氣到xx彈出來            
+            if (foodOtherItems.count == 0) {
+                refreshControl?.endRefreshing()
+                self.presentViewController(alertController, animated: true, completion: nil)
+                return
+            } else {
+                foodItems.insertObject(foodOtherItems[rand!], atIndex: 0)
+                foodOtherItems.removeObjectAtIndex(rand!)
+                foodTableVeiw.reloadData()
+                refreshControl?.endRefreshing()
+            }
+        }
+    }
 }
 
